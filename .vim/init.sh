8b93457e8afd3c345530cf4d46cc0ba58103d661
frq <frq@TAFF.frequentis.frq>
2015-06-02 14:51:12 +0200

    init.sh installs fonts now

--- .vim/init.sh
+++ .vim/init.sh
@@ -1,4 +1,17 @@
 #!/bin/bash
 
-git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-vim +PluginInstall +qall
+if [ -e ~/.vim/bundle/Vundle.vim ]; then
+   (cd ~/.vim/bundle/Vundle.vim
+   git pull)
+else
+   git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+fi
+vim +PluginInstall +PluginUpdate +qall
+
+if [ -e ~/.vim/fonts ]; then
+   (cd ~/.vim/fonts;
+   git pull)
+else
+   git clone git@github.com:powerline/fonts.git ~/.vim/fonts
+fi
+~/.vim/fonts/install.sh
